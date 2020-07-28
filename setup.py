5066554d0fe21f35c3d4ca034300f097524d1b7b
Rohit Khattar <rohitkh@byu.edu>
2020-07-28 12:55:29 -0600

    Minor setup.py cleanup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces'
 setup(
     name=release_package,
     version='0.0.1',
-    description='"A tethys app that lets the user to visualize and query WSDL enpoints',
+    description='A tethys app that lets the user to visualize and query WSDL enpoints',
     long_description='',
     keywords='',
     author='Giovanni Romero Bustamante',
