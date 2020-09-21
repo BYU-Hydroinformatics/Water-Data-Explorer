51e42fa0e0706358e507ef1db19ace83eb820eda
Elkin Romero <gio.busrom@gmail.com>
2020-09-21 14:50:27 -0600

    Js File for the logic site

---
+++ tethysapp/water_data_explorer/public/js/sites.js
@@ -0,0 +1,279 @@
+/*
+************ FUNCTION NAME: ACTIVATE_LAYER_VALUES **********************
+************ PURPOSE: THE FUNCTIONS RETRIEVES THE DATA FROM THE LAYERS WHEN ONE MAKES A CLICK ***********
+*/
+activate_layer_values = function (){
+  map.on('singleclick', function(evt) {
+    $('#variables_graph').selectpicker('setStyle', 'btn-primary');
+
+    evt.stopPropagation();
+    $("#graphs").empty();
+
+
+    let object_request={};
+    console.log(object_request);
+
+    var feature = map.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
+        //you can add a condition on layer to restrict the listener
+        if(feature){
+
+
+          if(layersDict['selectedPoint']){
+            map.removeLayer(layersDict['selectedPoint'])
+            delete layersDict[title]
+            map.updateSize()
+          }
+
+          let actual_Source = new ol.source.Vector({})
+          actual_Source.addFeature(feature);
+          let vectorLayer = new ol.layer.Vector({
+              source: actual_Source,
+              style:  new ol.style.Style({
+                  image: new ol.style.Circle({
+                      radius: 6,
+                      stroke: new ol.style.Stroke({
+                          color: "black",
+                          width: 4
+                      }),
+                      fill: new ol.style.Fill({
+                          color: `#FF0000`
+                      })
+                  })
+              })
+          })
+          layersDict['selectedPoint'] = vectorLayer;
+          console.log("adding new feature");
+          console.log(vectorLayer);
+          map.addLayer(vectorLayer);
+        }
+        return feature;
+        });
+    if (feature) {
+
+      initialize_graphs([],[],"No data Available","","","","scatter");
+
+
+      active_map_feature_graphs={
+        'scatter':{},
+        'bar':{},
+        'pie':{},
+        'whisker':{}
+      }
+      console.log(feature.values_['hs_name']);
+
+      $("#siteName_title").html(feature.values_['name']);
+      // object_request['hs_name']=feature.values_['hs_name'];
+      // object_request['site_name']=feature.values_['name'];
+      object_request['hs_url']=feature.values_['hs_url'];
+      object_request['code']=feature.values_['code'];
+      object_request['network']=feature.values_['network'];
+      // $("#plots").hide();
+      $("#graphAddLoading").css({left:'50%',bottom:"15%", position:'absolute',"z-index": 9999});
+      $("#graphAddLoading").removeClass("hidden");
+      console.log(object_request);
+
+      $.ajax({
+        type:"GET",
+        url: `get-values-hs`,
+        dataType: "JSON",
+        data: object_request,
+        success: function(result){
+          console.log(result);
+          let description_site = document.getElementById('siteDes')
+          console.log(description_site);
+          description_site.innerHTML =
+            ` <p> <em>Organization:</em> ${result['description'][Object.keys(result['description'])[0]]['organization']} <p>
+              <p> <em>Citation:</em> ${result['description'][Object.keys(result['description'])[0]]['citation']} <p>
+              <p> <em>Description:</em> ${result['description'][Object.keys(result['description'])[0]]['sourceDescription']}</p>`
+
+          let table_begin =
+        `<br>
+        <p>Table of Variables</p>
+        <table id="siteVariableTable" class="table table-striped table-hover table-condensed">
+            <tr class="danger">
+              <th>Variable</th>
+              <th>Code</th>
+              <th>Data Points</th>
+            </tr>`;
+          for(let i=0; i<result['variables'].length ; ++i){
+            let newRow =
+            `
+            <tr>
+              <th>${result['variables'][i]}</th>
+              <th>${result['codes'][i]}</th>
+              <th>${result['counts'][i]}</th>
+            </tr>
+            `
+            table_begin = table_begin + newRow;
+          }
+          table_begin = table_begin + `</table>`;
+          $("#table_div").html(table_begin);
+
+          active_map_feature_graphs['bar']['x_array']=[];
+          active_map_feature_graphs['pie']['x_array']=[];
+
+
+          // create Dict //
+          for(let i=0; i< result['variables'].length; ++i){
+            // let x_axis= `${result['variables'][i]} (${result['codes'][i]})`;
+            let x_axis= `${result['variables'][i]}`;
+            active_map_feature_graphs['bar']['x_array'].push(x_axis);
+            active_map_feature_graphs['pie']['x_array'].push(x_axis);
+
+          }
+          // let title_info = `${object_request['site_name']} Variables Distribution`;
+          let title_info = `${feature.values_['name']} Variables Distribution`;
+
+          // active_map_feature_graphs['bar']['x_array'] =result['variables'];
+          active_map_feature_graphs['bar']['y_array'] = result['counts'];
+          active_map_feature_graphs['bar']['type'] = 'bar';
+          active_map_feature_graphs['bar']['title_graph'] = title_info;
+
+          active_map_feature_graphs['pie']['y_array'] = result['counts'];
+          active_map_feature_graphs['pie']['type'] = 'pie';
+          active_map_feature_graphs['pie']['title_graph'] = title_info;
+
+
+
+          let check_empty_pieChart = true;
+          let check_array = [];
+
+          result['counts'].forEach(function(x){
+            if (x > 0){
+              check_empty_pieChart = true;
+              check_array.push(check_empty_pieChart);
+            }
+            else{
+              check_empty_pieChart = false;
+              check_array.push(check_empty_pieChart);
+
+            }
+
+          })
+          active_map_feature_graphs['pie']['check_none'] = check_array;
+          console.log(active_map_feature_graphs['pie']['check_none']);
+
+          if (check_array.includes(true)) {
+            initialize_graphs(active_map_feature_graphs['pie']['x_array'],result['counts'],title_info, undefined, undefined, undefined,'pie');
+          }
+          else{
+            initialize_graphs(['no variable has data'],[1],title_info, undefined, undefined, undefined,'pie');
+
+          }
+
+          evt.stopPropagation();
+          let object_code_and_variable = {};
+          let variables = result['variables'];
+          let code_variable =result['codes'];
+          codes_variables_array = JSON.parse(JSON.stringify(code_variable));
+          for(let i=0; i< variables.length ; ++i){
+            object_code_and_variable[`${variables[i]}`]=code_variable[i];
+          }
+          // console.log(object_code_and_variable);
+          let variable_select = $("#variables_graph");
+          variable_select.empty();
+          let i = 1;
+          let array_variables=[]
+          let option_variables;
+          // let variables_select = document.getElementById("variables_graph");
+          let option_beginning= `<option value= 0 selected= "selected" > Select an variable </option>`;
+          variable_select.append(option_beginning)
+
+          variables.forEach(function(variable){
+            let option;
+            let option_begin;
+              array_variables.push(variable);
+              if(i === 1){
+                console.log("initial");
+                // option = `<option selected = "selected>Variables Ready ..</option>`;
+                option_begin = `<option value=${i}>${variable} (${result['counts'][i-1]}) Data Points Counted </option>`;
+                variable_select.append(option_begin)
+
+              }
+              else{
+                option = `<option value=${i} >${variable} (${result['counts'][i-1]}) Data Points Counted</option>`;
+
+              }
+              variable_select.append(option)
+              // variable_select.append(option_variables)
+
+              variable_select.selectpicker("refresh");
+              i = i+1;
+          });
+          console.log(object_request);
+          console.log($('#variables_graph'));
+          let object_request2 = {};
+          object_request2['hs_name']=feature.values_['hs_name'];
+          object_request2['site_name']=feature.values_['name'];
+          object_request2['hs_url']=feature.values_['hs_url'];
+          object_request2['code']=feature.values_['code'];
+          object_request2['network']=feature.values_['network'];
+          //CONTINUE HERE // AND TRY TO SEE HOW IT GOES //
+          // console.log(object_request);
+          var selectedItem = $('#variables_graph')['0'].value;
+          var selectedItemText = $('#variables_graph')['0'].text;
+          console.log(selectedItem);
+
+          object_request2['variable']=selectedItem;
+          object_request2['code_variable']= code_variable[`${selectedItem}` -1];
+          object_request2['times_series'] = result['times_series']
+          object_request2['methodsIDs'] = result['methodsIDs']
+          object_request2['variables_array']=result['variables']
+          object_request_graphs = JSON.parse(JSON.stringify(object_request2));
+          console.log(object_request2);
+          let start_dateUTC = result['times_series'][Object.keys(result['times_series'])[0]]['beginDateTimeUTC']
+          console.log(start_dateUTC);
+          let dateUTC_start = new Date(start_dateUTC)
+          console.log(dateUTC_start);
+          let starts = start_dateUTC.split("T");
+          console.log(starts[0]);
+          // let start_date_ = starts[0]+ ' '+ starts[1]
+          let starts_no_seconds = starts[1].split(":");
+          // object_request_graphs["startTime_hhmmss"] = starts_no_seconds[0] +":" +starts_no_seconds[1];
+          let end_dateUTC = result['times_series'][Object.keys(result['times_series'])[0]]['endDateTimeUTC']
+          let dateUTC_end = new Date(end_dateUTC)
+
+          let ends = end_dateUTC.split("T");
+          console.log(ends[0]);
+
+          let ends_no_seconds = ends[1].split(":");
+          console.log(dateUTC_end);
+
+
+          console.log(new Date(starts[0]));
+          console.log(new Date(ends[0]));
+
+          // THIS IS NECESARRY TO RESET THE DATES OTHERWISE IT IS GOING TO HAVE EMPTY SPACES..
+          $('#datetimepicker6').datepicker('setStartDate', null);
+          $('#datetimepicker6').datepicker('setEndDate', null);
+          $('#datetimepicker7').datepicker('setEndDate',null);
+
+//               @KrunchMuffin I found a workaround this issue:
+//               Before setting the value remove the limitation (endDate)
+//               Set the value
+//               Restore the limitation (endDate)
+//
+//               Maybe it will work for you also
+//               https://github.com/uxsolutions/bootstrap-datepicker/issues/2292#issuecomment-341496634
+
+          $('#datetimepicker6').datepicker('update', dateUTC_start);
+          $('#datetimepicker7').datepicker('update', dateUTC_end);
+
+          console.log($('#datetimepicker6').datepicker('getDate'));
+          console.log($('#datetimepicker7').datepicker('getDate'));
+
+          $('#datetimepicker6').datepicker('setStartDate', dateUTC_start);
+          $('#datetimepicker6').datepicker('setEndDate', dateUTC_end);
+          // $('#datetimepicker7').datepicker('setStartDate',dateUTC_end);
+          $('#datetimepicker7').datepicker('setEndDate',dateUTC_end);
+           $("#graphAddLoading").addClass("hidden")
+
+
+        }
+
+      })
+
+    }
+  });
+
+}
