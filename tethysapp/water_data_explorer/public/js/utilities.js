e7d1e1bc79deb4bc71d7b8be6230ccf401bff14e
romer8 <gio.busrom@gmail.com>
2021-08-31 09:55:06 -0600

    makign the buttons extra small

--- tethysapp/water_data_explorer/public/js/utilities.js
+++ tethysapp/water_data_explorer/public/js/utilities.js
@@ -625,27 +625,29 @@ change_effect_groups = function(element_to_check,id_group_separator){
   * @return {string} newHtml - html string containing the catalogs structure.
 * */
 html_for_servers = function(title,group_name,isNew){
+
+
   try{
+
     let check_var = (( isNew == true ) ? 'checked' : '');
     let newHtml = `
     <li class="ui-state-default" layer-name="${title}" id="${title}" >
     <span class="server-name tool_tip_h" data-toggle="tooltip" data-placement="right" title="${id_dictionary[title]}">${id_dictionary[title]}</span>
     <input class="chkbx-layer" type="checkbox" data-toggle="tooltip" data-placement="bottom" title="Show/Hide View" ${check_var}>
-    <button type="button" id="${title}_${group_name}_reload" class="btn btn-sm" >
+    <button type="button" id="${title}_${group_name}_reload" class="btn btn-xs" >
      <span  class="glyphicon glyphicon-refresh tool_tip_h" aria-hidden="true" data-toggle="tooltip" data-placement="bottom" title="Update View">
      </span>
     </button>
-    <button type="button" id="${title}_zoom" class="btn btn-dark btn-sm" >
+    <button type="button" id="${title}_zoom" class="btn btn-dark btn-xs" >
      <span class="glyphicon glyphicon-map-marker tool_tip_h" aria-hidden="true" data-toggle="tooltip" data-placement="bottom" title="Zoom to View"></span>
     </button>
 
-    <button id="${title}_variables" class="btn btn-dark btn-sm" data-toggle="modal" data-target="#modalShowVariablesTable"> <span class=" glyphicon glyphicon-list-alt tool_tip_h" data-toggle="tooltip" data-placement="bottom" title="View Variables"></span>
+    <button id="${title}_variables" class="btn btn-dark btn-xs" data-toggle="modal" data-target="#modalShowVariablesTable"> <span class=" glyphicon glyphicon-list-alt tool_tip_h" data-toggle="tooltip" data-placement="bottom" title="View Variables"></span>
     </button>
 
-    <button type="button" id="${title}_variables_info" class="btn btn-dark btn-sm" data-toggle="modal" data-target="#modalHydroserInformation">
+    <button type="button" id="${title}_variables_info" class="btn btn-dark btn-xs" data-toggle="modal" data-target="#modalHydroserInformation">
      <span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>
     </button>
-
 
     </li>
     `;
