aed334f82050f2077bffc65a7a2acd019b57f03d
romer8 <gio.busrom@gmail.com>
2021-10-18 14:48:26 -0600

    automatic check if thereis an amazon database or not.

--- docker_files/final_run.sh
+++ docker_files/final_run.sh
@@ -1,6 +1,14 @@
 #!bin/bash
-echo 'Running the external database configuration . . .'
-/bin/bash configure_db.sh
-echo 'Finished database configuration . . .'
-echo 'Running salt scripts for the configuration of the app . . . '
-/bin/bash run.sh
+
+SUB='rds.amazonaws.com'
+if [[ "$TETHYS_DB_HOST" =~ .*"$SUB".* ]];
+then
+  echo 'Running the external database configuration . . .'
+  /bin/bash configure_db.sh
+  echo 'Finished database configuration . . .'
+  echo 'Running salt scripts for the configuration of the app . . . '
+  /bin/bash run.sh
+else
+  echo 'Running salt scripts for the configuration of the app . . . '
+  /bin/bash run.sh
+fi
